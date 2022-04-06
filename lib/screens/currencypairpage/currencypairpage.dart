85820ba3062f36dbfe8cfed2406ce3ae1d2d5daf
Anthony Aniobi <anthonyaniobi198@gmail.com>
2022-04-06 17:15:01 +0100

    added stream builder

--- lib/screens/currencypairpage/currencypairpage.dart
+++ lib/screens/currencypairpage/currencypairpage.dart
@@ -1,1 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:forexmonitor/network/app_sockets/appsockets.dart';
+
+class CurrencyPairPage extends StatefulWidget {
+  const CurrencyPairPage({Key? key}) : super(key: key);
+
+  @override
+  _CurrencyPairPageState createState() => _CurrencyPairPageState();
+}
+
+class _CurrencyPairPageState extends State<CurrencyPairPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder(
+          stream: AppSockets().streamData(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasError) {
+              return Text(snapshot.data.toString());
+            }
+            return Text('Error');
+          }),
+    );
+  }
+}
